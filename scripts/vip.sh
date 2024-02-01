10b8d29e7a39542e0086890f22517ab81f695177
Hermes Ribeiro <hermes.ribeiro@gmail.com>
2024-01-31 21:16:22 -0300

    Working version with taskfiles

---
+++ scripts/vip.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+docker run --network host --rm ghcr.io/kube-vip/kube-vip:v0.6.4 \
+   	manifest pod \
+    --interface lo \
+	--address $2 \
+    --controlplane \
+	--services \
+    --bgp \
+	--localAS 65000 \
+	--bgpRouterID $1 \
+	--bgppeers $3:65000::false,$4:65000::false,$5:65000::false,$6:65000::false \
+	| sudo tee /etc/kubernetes/manifests/kube-vip.yaml
